811494c389f3262365471e80b87c3083a810ca12
Molly Bohmer <60149332+mbohmer@users.noreply.github.com>
2023-07-06 19:51:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title('My parents healthy diner')
-streamlit.header('Menu')
-streamlit.text('Delicious Food')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
