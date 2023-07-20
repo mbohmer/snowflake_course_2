16635ed409f7e957e94a4246ef48b8e81c299ff4
Molly Bohmer <60149332+mbohmer@users.noreply.github.com>
2023-07-20 09:07:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,6 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     steamlit.dataframe(back_from_function)
 
-except URLError as e:
-  streamlit.error()
-
 
 #streamlit.write('The user entered ', fruit_choice)
 #streamlit.text(fruityvice_response.json())
